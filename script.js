abe22b77a794ed7f6bd3fc2402abc4646c857672
Danny Vega <dannyevega@gmail.com>
2015-10-07 13:37:48 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function fizzBuzz(num){
+	var current = 0;
+	for(var i = 1; i <= num; i++, current++){
+		if(i % 3 === 0 && i % 5 === 0){
+			$('#fizzbuzz').append('<p>FizzBuzz</p>');
+		} else if(i % 3 === 0){
+			$('#fizzbuzz').append('<p>Fizz</p>');
+		} else if(i % 5 === 0){
+			$('#fizzbuzz').append('<p>Buzz</p>');
+		} else {
+			$('#fizzbuzz').append('<p>' + i + '</p>');
+		}
+	}	
+}
+
+fizzBuzz(100);
